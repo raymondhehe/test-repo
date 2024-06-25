3c8ea31b34931cad5dec667f0f30b62bfb76a610
Raymond <mr.raymondsi@gmail.com>
2024-06-25 11:28:59 +0200

    Update missing_colon.py

--- tests/missing_colon.py
+++ tests/missing_colon.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
 import os
 
+# send the output to output.txt
 def division(a: float, b: float) -> float
     return a/b
-
 
 if __name__ == "__main__":
     print(division(123, 15))
